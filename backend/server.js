04e8ad75ffd09e69378579c4c0c890e4910d0875
Tejas Dewett <91184807+TejasDewett@users.noreply.github.com>
2023-02-04 12:01:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
@@ -14,13 +15,21 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'JSON'})
 })
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve Frontend
+if(process.env.NODE_ENV == 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+    res.status(200).json({message: 'JSON'})
+}
 
 app.use(errorHandler)
 
