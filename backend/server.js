2374313b05dc028962a39046126bfa4c46882e34
Tejas Dewett <91184807+TejasDewett@users.noreply.github.com>
2023-02-17 17:59:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'JSON'})
 })
 
 //Routes
