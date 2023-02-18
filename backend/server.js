070d8335220c1d05d715e390f549a3c62e791a1c
Tejas Dewett <91184807+TejasDewett@users.noreply.github.com>
2023-02-18 14:01:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
@@ -25,6 +26,15 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 
 
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 
 app.use(errorHandler)
